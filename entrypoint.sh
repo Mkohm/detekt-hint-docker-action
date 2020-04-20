99a8ce3fe53ae8f7dbda8ee0d05f90ce0710233c
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-20 11:08:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ echo "::set-env name=DANGER_GITHUB_API_TOKEN::$1"
 
 git clone --depth=50 --branch=master https://github.com/Mkohm/detekt.git Mkohm/detekt
 ./gradlew assemble
+gem install ruby-ll -v '2.1.2' --source 'https://rubygems.org/'
 bundle update
 bundle install
 bundle update danger
