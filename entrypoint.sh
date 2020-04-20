f940d4e6c8700d54b2fb4d022a0e9deef058d2a1
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-20 10:30:22 +0200

    try fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,7 @@ echo "::set-output name=time::$time"
 
 echo "::set-env name=DANGER_GITHUB_API_TOKEN::$1"
 
-
-
+git clone --depth=50 --branch=master https://github.com/Mkohm/detekt.git Mkohm/detekt
 ./gradlew assemble
-gem install bundler
-bundle install
 bundle update danger
 bundle exec danger --verbose
