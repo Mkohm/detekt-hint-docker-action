eb7fcf62b55177091422e86c3f6a80e0ee0e6c90
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-20 22:30:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ echo "::set-output name=time::$time"
 
 echo "::set-env name=DANGER_GITHUB_API_TOKEN::$1"
 
+echo "Github Workspace is: $GITHUB_WORKSPACE"
+echo "ls $GITHUB_WORKSPACE"
+echo "Github Home is: $HOME"
+echo "ls $HOME"
+
 
 ./gradlew assemble
 bundle update
