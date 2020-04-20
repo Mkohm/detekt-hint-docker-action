3f238e7ae6f1e8a2a0a37129ee2d0cfbce3e9ca5
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-20 14:42:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ echo "::set-env name=DANGER_GITHUB_API_TOKEN::$1"
 
 ./gradlew assemble
 bundle update
-bundle install --gemfile=/github/workspace/
+bundle install --gemfile=/github/workspace/Gemfile
 bundle update danger
 bundle exec danger --verbose
