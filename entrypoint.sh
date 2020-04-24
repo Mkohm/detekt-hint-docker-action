a63386b748aa51ec1095fec53598c2a162050138
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 12:13:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ ls
 echo "pwd"
 pwd
 
+# Install danger-kotlin
+bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
+source ~/.bash_profile
+
 npx --package danger danger-kotlin ci --dangerfile /Dangerfile.df.kts
