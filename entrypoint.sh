599575e9371afc12cfa9fa75c81243bf6d8327f7
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 08:59:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,16 @@ source /etc/environment
 echo "New java home:"
 echo $JAVA_HOME
 
-./gradlew assemble
-bundle update
-bundle install --gemfile=/Gemfile
-bundle update danger
 
-# Will always return a successfull exit code, making PRs from forks also succeed. However, because of this bug https://github.com/danger/danger/issues/1103 Danger will not run. When the bug is fixed, the command can be run as normally.
-bundle exec danger --verbose --dangerfile=/Dangerfile
+bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
+source ~/.bash_profile
+
+# Build artifacts
+./gradlew assemble
+
+# Run detekt hint to generate the warning report
+./gradlew detektHint
+
+# Run danger/kotlin
+danger-kotlin ci
+
