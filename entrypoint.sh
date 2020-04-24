a2d07b683c3038dd024f8a8aaaac685433a4b029
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 11:56:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-
-echo "::set-env name=DANGER_GITHUB_API_TOKEN::$1"
-
-
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
-source /etc/environment
-echo "New java home:"
-echo $JAVA_HOME
-
-yes | apt install curl
-
+echo "ls"
+ls
+echo "pwd"
+pwd
 
 bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
 source ~/.bash_profile
 
-# Build artifacts
-./gradlew assemble
-
-# Run detekt hint to generate the warning report
-./gradlew detektHint
-
-# Run danger/kotlin
-danger-kotlin ci
-
+npx --package danger danger-kotlin ci --dangerfile /Dangerfile.df.kts
